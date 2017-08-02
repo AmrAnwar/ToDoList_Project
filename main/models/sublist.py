e0b808c2350bd5af579fc62d9df2aed341cbc137
AmrAnwar <amranwar945@gmail.com>
2017-08-02 17:24:43 +0200

    admin finished

--- main/models/sublist.py
+++ main/models/sublist.py
@@ -10,7 +10,6 @@ class Sublist(models.Model):
     title = models.CharField(null=False, blank=False, max_length=100)
     archived = models.BooleanField(default=False)
     timestamp = models.DateTimeField(auto_now_add=True)
-
     task = models.ForeignKey(Task, related_name="task_sublist")
 
     class Meta:
--- main/models/task.py
+++ main/models/task.py
@@ -3,11 +3,12 @@ from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.db import models
 from .list import List
+from django.conf import settings
 # Create your models here.
 
 
 class Task(models.Model):
-    user = models.ForeignKey(User, related_name="task_users")
+    user = models.ForeignKey(User, default=1, related_name="task_users")
     title = models.CharField(null=False, blank=False, max_length=100)
     archived = models.BooleanField(default=False)
     timestamp = models.DateTimeField(auto_now_add=True)
